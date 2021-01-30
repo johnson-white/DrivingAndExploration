ce61b5f3f522ac9925ff8dc291e5fb10eee40ed5
js <placeholdercommitsemail@test.com>
2021-01-30 16:04:21 +0000

    Forward/Reverse drive using AddForce with the limitation that the object is locked on it's Y axis to prevent it toppling over forwards/backwards due to friction.

--- Assets/Scripts/MoveVehicle.cs
+++ Assets/Scripts/MoveVehicle.cs
@@ -1,25 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveVehicle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Visible Params
+    public float accelerationForce = 1f;
+    public Rigidbody vehicle;
+    
     void Start()
     {
-        
+        // Assign component first
+        vehicle = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() // consistent 0.2 ms physics updates
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If button pressed. Keys (e.g. "Vertical") are managed in Unity/Edit/Project Settings/Input Manager
         if (Input.GetButton("Vertical"))
         {
-            /* Changing the position of the script's parent object by getting the positive or negative Vertical input,
-            which is a float, then multiplying this float with a Forward Z axis value that Unity has abstracted in place
-            of (0, 0, 1) */
-            transform.position += Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime;
+            // Can call this from Update() for good input detection, and the effects will take place on the next FixedUpdate()
+            vehicle.AddForce(transform.forward * accelerationForce * Input.GetAxis("Vertical"));
+            // Unfortunately friction causes this to rotate on its Y axis and topple over forwards/backwards
+            // Temp workaround of freezing the Y axis rotation on the object for now. Maybe look into using wheel colliders instead.
         }
         
         if (Input.GetButton("Horizontal"))
