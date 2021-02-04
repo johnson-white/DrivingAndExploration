82b71ea0c62af49f704ac001048383776a7ac300
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2021-02-04 16:20:50 +0000

    Refactored methods for readability. Implemented a grounding check but the results are unreliable. To be improved.

--- Assets/Scripts/MoveVehicle.cs
+++ Assets/Scripts/MoveVehicle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MoveVehicle : MonoBehaviour
@@ -21,6 +22,13 @@ public class MoveVehicle : MonoBehaviour
 
     void Update()
     {
+        bool g = Grounded();
+        Debug.Log(g);
+        Movement();
+    }
+
+    private void Movement()
+    {
         if (Input.GetButton("Vertical"))
         {
             vehicle.AddForce(transform.forward * accelerationForce * Input.GetAxis("Vertical"), ForceMode.Acceleration);
@@ -29,11 +37,24 @@ public class MoveVehicle : MonoBehaviour
         if (Input.GetButton("Horizontal"))
         {
             Vector3 localVelocity = transform.InverseTransformDirection(vehicle.velocity); //global velocity transformed to local velocity
-            Debug.Log("Z axis speed value is: " + localVelocity.z);
+            // Debug.Log("Z axis speed value is: " + localVelocity.z);
             float direction = (localVelocity.z > 0) ? 1f : -1f; //if z axis velocity is negative, invert torque direction.
             vehicle.AddRelativeTorque(transform.up * rotationForce * Input.GetAxis("Horizontal") * direction);
         }
-        
-        
     }
+
+    private bool Grounded() //cast ray down from vehicle to check if it's grounded
+    {
+        Transform vehicleTransform;
+        Vector3 down = new Vector3(0f, -0.3f, 0f);
+        Vector3 pointDir = (vehicleTransform = vehicle.transform).TransformDirection(down);
+        // Debug.Log("<color=yellow>pointDir - " + pointDir + "</color>");
+        Vector3 vehiclePosition = vehicleTransform.position; //using centre bottom of vehicle, can implement for wheel traction if wanted in the future
+        // Debug.Log("<color=red>vehiclePosition - " + vehiclePosition + "</color>");
+        Debug.DrawRay(vehiclePosition, pointDir, Color.red);
+        
+        RaycastHit hit1;
+        return Physics.Raycast(vehiclePosition, pointDir, out hit1, 0.3f);
+    }
+    
 } 
